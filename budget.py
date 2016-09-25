540828adde87f62390079b102b330e51018194c4
HelinaBerhane <fae1029@hotmail.co.uk>
2016-09-25 12:45:48 +0100

    Update budget.py

--- budget.py
+++ budget.py
@@ -7,28 +7,30 @@ import sqlite3 # databases
 ### Initialisation ###
 
 # create the connection object that represents the database
-connextion = sqlite3.connect('budget.db')
+connection = sqlite3.connect('budget.db')
 # create a Cursor object and call its execute() method to perform SQL commands:
 c = connection.cursor()
 
 ## In ##
 # Create the income table
 c.execute("CREATE TABLE IF NOT EXISTS incomeTable(date INT, amount FLOAT, dailyAmount FLOAT, notes STRING(1000))")
+
 # Ask to add income
 askI = input("Do you want to add any incomes? y/n")
 # Add income
 if askI == "y":
     iStr    = input("Input date, amount, notes")
     iArr    = iStr.split(", ")
-	iDate   = incArr[0]
-	iAmount = incArr[1]
-	iDaily  = str(float(iAmount)/365.25)
-	iNotes  = incArr[2]
-    c.execute("INSERT INTO income (date, amount, notes) VALUES ("+iDate+", "+iAmount[1]+", "+incArr[2]+")") 
+    fIArr   = [iArr[0], iArr[1], str(float(iArr[1])/365), iArr[2]]
+	#iDate   = iArr[0]
+	#iAmount = iArr[1]
+	#iDaily  = str(float(iAmount)/365.25)
+	#iNotes  = incArr[2]
+    c.execute("INSERT INTO incomeTable VALUES (?,?,?,?)", fIArr)
     # this doesnt work atm
 # Print incomeTable
-for row in c.execute("SELECT * FROM income"):
-	print( i )
+for row in c.execute("SELECT * FROM incomeTable"):
+	print( row )
 
 ## Out ##
 # Create the spendings table
@@ -37,9 +39,9 @@ for row in c.execute("SELECT * FROM income"):
 #askSpendings = input("Do you want to add any spendings? y/n")
 # Add income
 #if askSpendings == "y":
-#    spendingsStr = input("Input date, amount, notes")
-#    spendArr     = spendingsStr.split(", ")
-#    c.execute("INSERT INTO spendings (date, amount, notes) VALUES ("+incArr[0]+", "+incArr[1]+", "+incArr[2]+")") 
+    #spendingsStr = input("Input date, amount, notes")
+    #spendArr     = spendingsStr.split(", ")
+    #c.execute("INSERT INTO spendings (date, amount, notes) VALUES ("+incArr[0]+", "+incArr[1]+", "+incArr[2]+")") 
     # this doesnt work atm
 # Print incomeTable
 #spendingsData = c.execute( "SELECT * FROM spendings" )
@@ -67,5 +69,5 @@ for row in c.execute("SELECT * FROM income"):
 ### End ###
 
 # Commit all changes to the database and close the connection
-    # Save (commit) the changes:    conn.commit()
-    # Close the connection:         conn.close()
+connection.commit()   # Save (commit) the changes:
+connection.close()    # Close the connection
